aeacba6120538d90a8a682b9a046bd45dd6b574c
daencer <33512814+daencer@users.noreply.github.com>
2017-11-28 22:56:49 +0100

    nicest Version of the dawn World #pwned

--- elevators.py
+++ elevators.py
@@ -10,14 +10,15 @@ __credits__ = "nobody"
 __email__ = "s1539940@stud.uni-frankfurt.de"
 
 class Lift(object):
-    def __init__(self, jobs = [], pos = 0, way = 's'):
+    def __init__(self, jobs = [], pos = 0, way = 's',move = 'not'):
         self.jobs = jobs
         self.pos = pos
         self.way = way
-    def move(self):
-        if self.way == 'h':
+        self.move = move
+    def getmove(self, move):
+        if move == 'up':
             self.pos += 1
-        if self.way == 'r':
+        if move == 'down':
             self.pos -= 1
         if self.pos in self.jobs:
             self.jobs.remove(self.pos)
@@ -33,7 +34,10 @@ class Lift(object):
     def setway(self, w):
         self.way = w
     def setpos(self, p):
-        self.pos = p
+        if p == "up":
+            self.pos += 1
+        if p == "down":
+            self.pos -= 1
 
 def printstats(l, lift_a, lift_b):
     cout = 'A faehrt '
@@ -43,8 +47,6 @@ def printstats(l, lift_a, lift_b):
         cout += 'runter'
     if lift_a.getway() == 's':
         cout = 'A steht'
-    if lift_a.getway() == 'o':
-        cout = 'A ist offen'
     print(cout)
     cout = 'B faehrt '
     if lift_b.getway() == 'h':
@@ -53,8 +55,6 @@ def printstats(l, lift_a, lift_b):
         cout += 'runter'
     if lift_b.getway() == 's':
         cout = 'B steht'
-    if lift_b.getway() == 'o':
-        cout = 'B ist offen'
     print(cout)
 
 #niceigkeit
@@ -97,26 +97,108 @@ def newrequest(i, l):
 
 
 
-lift_a = Lift()
-lift_b = Lift()
+lift_a = Lift([])
+lift_b = Lift([])
 requests = []
+
 levels = ['K', 'E', '1', '2', '3', '4']
 # Liste für direction
 dir_a = []
 dir_b = []
 
 while True:
+    req = []
     visualize(levels, lift_a, lift_b)
     cin = input('--> ')
     req = newrequest(cin, levels)
-    print(newrequest(cin, levels))
-    if cin == "":
-        # befehl rein damit Tick eins weiter läuft also ohne irgendwas zu machen
-        continue
     if len(req) > 0:
         requests.append(req)
-    if len(lift_a.getjobs()) <= len(lift_b.getjobs()):
-        lift_a.jobs.append(requests)
-    else:
-        lift_b.jobs.append(requests)
+        print("success")
+        if len(lift_a.getjobs()) <= len(lift_b.getjobs()):
+            lift_a.newjob(requests[0])
+            requests.pop(0)
+            '''
+            newlist = lift_a.jobs[0]
+            print(newlist)
 
+            
+            if lift_a.pos < newlist[0]:
+                lift_a.setway("h")
+                lift_a.setpos("up")
+                print(lift_a.getpos())
+            elif lift_a.pos > newlist[0]:
+                lift_a.setway("r")
+            else:
+                lift_a.setway("s")
+            '''
+
+
+            #lift_a.getmove()
+
+
+        else:
+            lift_b.newjob(requests[0])
+            requests.pop(0)
+            newlist = lift_b.jobs[0]
+            print(newlist)
+            if lift_b.pos < newlist[0]:
+                lift_b.setway("h")
+            elif lift_b.pos > newlist[0]:
+                lift_b.setway("r")
+            else:
+                lift_b.setway("s")
+           # lift_b.getmove()
+    else:
+        print("<return> gedrückt")
+
+    if len(lift_a.getjobs()) > 0:
+        newlist_a = lift_a.jobs[0]
+        print(newlist_a)
+        if lift_a.pos < newlist_a[0]:
+            lift_a.setway("h")
+            lift_a.setpos("up")
+            print(lift_a.getpos())
+        elif lift_a.pos > newlist_a[0]:
+            lift_a.setway("r")
+            lift_a.setpos("down")
+        else:
+            lift_a.setway("s")
+            lift_a.jobs.pop(0)
+            cabine_input = input("Where to go(A): ")
+            if cabine_input != "":
+                for i in cabine_input.split(','):
+                    tmp = []
+                    tmp.append(levels.index(i))
+                    lift_a.jobs.insert(0, tmp)
+                    print("TEMP=", tmp)
+
+    if len(lift_b.getjobs()) > 0:
+        newlist_b = lift_b.jobs[0]
+        print(newlist_b)
+        if lift_b.pos < newlist_b[0]:
+            lift_b.setway("h")
+            lift_b.setpos("up")
+        elif lift_b.pos > newlist_b[0]:
+            lift_b.setway("r")
+            lift_b.setpos("down")
+        else:
+            lift_b.setway("s")
+            lift_b.jobs.pop(0)
+            cabine_input = input("Where to go(B): ")
+            if cabine_input != "":
+                for i in cabine_input.split(','):
+                    tmp = []
+                    tmp.append(levels.index(i))
+                    lift_b.jobs.append(tmp)
+                    print("TEMP=", tmp)
+
+    print("reg= ", req)
+    print("requests=", requests)
+    print("lift_a.jobs=",lift_a.jobs )
+    print("lift_b.jobs=", lift_b.jobs)
+
+
+
+
+
+    
