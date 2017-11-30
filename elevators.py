94808c2732800267977532c3951151e3b9666656
Markus <mkalusche@me.com>
2017-11-30 14:52:37 +0100

    script separated in two modules

--- elevators.py
+++ elevators.py
@@ -2,6 +2,7 @@
 Two-Elevator-Simulation
 from EPR-Job No.4
 """
+from userinterface import visualize
 import random
 # import nothing
 __author__ = "6598273: Markus Kalusche, 6768647: Tobias Denzer"  # your data
@@ -58,35 +59,6 @@ def printstats(l, lift_a, lift_b):
         cout = 'B steht'
     print(cout)
 
-def visualize(l, lift_a, lift_b):
-    i = len(l) - 1
-    print('⌈ - A -- -- B - ⌉')
-    while i >= 0:
-        cout = '⏐ '
-        cout += l[i] + ' '
-        if lift_a.getpos() == i:
-            if lift_a.getway() == 's':
-                cout += '[ ]'
-            if lift_a.getway() == 'h':
-                cout += '/\ '
-            if lift_a.getway() == 'r':
-                cout += '\/ '
-        else:
-            cout += '   '
-        cout += ' | '
-        if lift_b.getpos() == i:
-            if lift_b.getway() == 's':
-                cout += '[ ]'
-            if lift_b.getway() == 'h':
-                cout += '/\ '
-            if lift_b.getway() == 'r':
-                cout += '\/ '
-        else:
-            cout += '   '
-        cout += ' ' + l[i] + ' ⏐'
-        print(cout)
-        i -= 1
-    print('⌊ - A -- -- B - ⌋')
 def randtick():
     x = random.randint(1, 3)
     return x
